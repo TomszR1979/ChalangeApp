cbbb6513f7ba66ce8e996531e04ab31284ada692
Tomasz Rakoczy <tomes282@gmail.com>
2023-02-28 23:16:22 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+string name = "Ewa";
+var age = 33;
+bool women = true;
+
+if (women = true && age < 30) 
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+
+else if(name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa ma 33 lata");
+}
+else if(women == false && age > 40)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
